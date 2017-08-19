1817a1af37aad0f8408b84610bfed07524f8c4aa
RipleyTom <RipleyTom@users.noreply.github.com>
2017-08-19 17:35:19 +0200

    No Warning

--- rpcs3/rpcs3qt/main_window.cpp
+++ rpcs3/rpcs3qt/main_window.cpp
@@ -121,18 +121,18 @@ void main_window::Init()
 	if (true)
 #endif
 	{
-		QMessageBox msg;
-		msg.setWindowTitle("Experimental Build Warning");
-		msg.setIcon(QMessageBox::Critical);
-		msg.setTextFormat(Qt::RichText);
-		msg.setText("Please understand that this build is not an official RPCS3 release.<br>This build contains changes that may break games, or even <b>damage</b> your data.<br>It's recommended to download and use the official build from <a href='https://rpcs3.net/download'>RPCS3 website</a>.<br><br>Build origin: " STRINGIZE(BRANCH) "<br>Do you wish to use this build anyway?");
-		msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-		msg.setDefaultButton(QMessageBox::No);
-		
-		if (msg.exec() == QMessageBox::No)
-		{
-			std::exit(EXIT_SUCCESS);
-		}
+		//QMessageBox msg;
+		//msg.setWindowTitle("Experimental Build Warning");
+		//msg.setIcon(QMessageBox::Critical);
+		//msg.setTextFormat(Qt::RichText);
+		//msg.setText("Please understand that this build is not an official RPCS3 release.<br>This build contains changes that may break games, or even <b>damage</b> your data.<br>It's recommended to download and use the official build from <a href='https://rpcs3.net/download'>RPCS3 website</a>.<br><br>Build origin: " STRINGIZE(BRANCH) "<br>Do you wish to use this build anyway?");
+		//msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+		//msg.setDefaultButton(QMessageBox::No);
+		//
+		//if (msg.exec() == QMessageBox::No)
+		//{
+		//	std::exit(EXIT_SUCCESS);
+		//}
 	}
 }
 
