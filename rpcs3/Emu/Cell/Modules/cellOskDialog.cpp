a5787acc2768c2a2c47d01cf8987b16b9c463b9c
RipleyTom <RipleyTom@users.noreply.github.com>
2017-08-21 11:12:00 +0200

    Takes into account text limit and initial text

--- rpcs3/Emu/Cell/Modules/cellOskDialog.cpp
+++ rpcs3/Emu/Cell/Modules/cellOskDialog.cpp
@@ -14,7 +14,13 @@ s32 cellOskDialogLoadAsync(u32 container, vm::ptr<CellOskDialogParam> dialogPara
 {
 	cellOskDialog.warning("cellOskDialogLoadAsync(container=0x%x, dialogParam=*0x%x, inputFieldInfo=*0x%x)", container, dialogParam, inputFieldInfo);
 
+	u32 maxLength = inputFieldInfo->limit_length >= 512 ? 511 : inputFieldInfo->limit_length;
+
 	std::memset(s_osk_text, 0, sizeof(s_osk_text));
+
+	if (inputFieldInfo->init_text.addr() != 0)
+		for (u32 i = 0; (i < maxLength) && (inputFieldInfo->init_text[i] != 0); i++)
+			s_osk_text[i] = inputFieldInfo->init_text[i];
 
 	const auto osk = Emu.GetCallbacks().get_msg_dialog();
 	bool result = false;
@@ -33,7 +39,7 @@ s32 cellOskDialogLoadAsync(u32 container, vm::ptr<CellOskDialogParam> dialogPara
 
 	Emu.CallAfter([&]()
 	{
-		osk->CreateOsk("On Screen Keyboard", s_osk_text);
+		osk->CreateOsk("On Screen Keyboard", s_osk_text, maxLength);
 	});
 
 	sysutil_send_system_cmd(CELL_SYSUTIL_OSKDIALOG_LOADED, 0);
